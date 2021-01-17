71a16e8a9606191f23510bbb584c0a2b8a779471
dev <hizlle1817@gmail.com>
2021-01-18 00:05:12 +0500

    update logic db

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,8 @@
 import dao.BooksDAO;
 import entity.BookModel;
-import model.BooksDaoImpliments;
+import model.ModelImplementation;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,22 +10,57 @@ public class Application {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        boolean running = true;
+
+        while (running) {
+            System.out.println("Menu");
+
+            BooksDAO booksDAO = new ModelImplementation();
+            List<BookModel> bookModels = booksDAO.findAll() ;
+
+            String command = scanner.nextLine();
 
 
-        while (true) {
-            BooksDAO booksDao = new BooksDaoImpliments();
+            switch (command) {
+//
+//                case ("save") : {
+//
+//                   bookModels.setTitle()
+//                    booksDAO.save()
+//
+//
+//                    break;
+//                }
+//
+                
 
-            List<BookModel> findBooks = booksDao.findAll();
-            findBooks.forEach(System.out::println);
+                case ("find"): {
 
-            System.out.println("****************************\n");
+                    List<BookModel> findBooks = booksDAO.findAll();
+                    findBooks.forEach(System.out::println);
 
-            System.out.println(booksDao.findBy(scanner.nextInt()));
-            //you can hardcode the value
+                    System.out.println("****************************\n");
+                    break;
+                }
+                case ("findby"): {
 
-            System.out.println("****************************\n");
+                    System.out.println(booksDAO.findBy(scanner.nextInt()));
 
 
+                    System.out.println("****************************\n");
+                    break;
+                }
+                case ("delete"): {
+                    booksDAO.delete(scanner.nextInt());
+                    System.out.println("delete\n");
+                    break;
+                }
+
+                case ("exit"): {
+                    running = false;
+                    System.out.println("exit");
+                }
+            }
         }
     }
 }
